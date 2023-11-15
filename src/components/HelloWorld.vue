605b52469508bd9607b5f3f97ea303326764430d
luoluoxue <473887719@qq.com>
2023-11-15 14:51:46 +0800

    update:修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,11 @@
 <template>
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HelloWorld l',
   props: {
     msg: String
   },
